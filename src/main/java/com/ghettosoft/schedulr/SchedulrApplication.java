4013e22131ec4d46d26a5653a80b8b586a9512d8
Sambit Nanda <snanda@manh.com>
2018-01-22 20:32:31 -0500

    Initial commit.

---
+++ src/main/java/com/ghettosoft/schedulr/SchedulrApplication.java
@@ -0,0 +1,12 @@
+package com.ghettosoft.schedulr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ghettosoft/schedulr/SchedulrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ghettosoft.schedulr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchedulrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
